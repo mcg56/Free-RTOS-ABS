20db22f71454ae0a61acd2905f6c5be578e6c024
Anton <amu118@uclive.ac.nz>
2022-07-21 18:45:50 +1200

    OLED working in car.c

--- apps/car/car.c
+++ apps/car/car.c
@@ -9,6 +9,9 @@
 #include <FreeRTOS.h>
 #include <task.h>
 #include "ap_pwm.h"
+#include "OrbitOLEDInterface.h"
+#include "stdlib.h"
+#include "utils/ustdlib.h"
 #include "driverlib/pwm.h"
 
 void blink(void* args) {
@@ -23,11 +26,24 @@ void blink(void* args) {
         // configASSERT(wake_time < 1000);  // Runs vAssertCalBled() if false
     }
 }
-
+void
+initDisplay (void)
+{
+  // intialise the Orbit OLED display
+	OLEDInitialise ();
+}
+void displayButtonState (char *butStr, char *stateStr, uint8_t numPushes, uint8_t charLine)
+{
+    char string[17]; // Display fits 16 characters wide.
+	
+    OLEDStringDraw ("                ", 0, charLine);
+    usnprintf (string, sizeof(string), "%s - %s %2d", butStr, stateStr, numPushes);
+    OLEDStringDraw (string, 0, charLine);
+}
 
 int main(void) {
     SysCtlClockSet (SYSCTL_SYSDIV_2_5 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN | SYSCTL_XTAL_16MHZ);
-
+    initDisplay ();
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
     GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_1);
 
@@ -39,6 +55,11 @@ int main(void) {
 
     // Initialisation is complete, so turn on the output.
     PWMOutputState(PWM_MAIN_BASE, PWM_MAIN_OUTBIT, true);
+
+    
+    int8_t upPushes = 2;
+
+    displayButtonState ("DOWN ", "RELS", upPushes, 1);
 
     while(1)
     {
@@ -60,3 +81,4 @@ void vAssertCalled( const char * pcFile, unsigned long ulLine ) {
     (void)ulLine; // unused
     while (true) ;
 }
+
--- libs/libs.mk
+++ libs/libs.mk
@@ -1,16 +1,17 @@
 
-SRC += libs/ap_pwm.c
+SRC += libs/ap_pwm.c 
+SRC += libs/OrbitOLEDInterface.c
 INCLUDES += -I"$(PROJECT_DIR)/libs/include"
 
-# SRC += libs/OrbitOLED/OrbitOLEDInterface.c
-
-# SRC += libs/OrbitOLED/lib_OrbitOled/ChrFont0.c
-# SRC += libs/OrbitOLED/lib_OrbitOled/delay.c
-# SRC += libs/OrbitOLED/lib_OrbitOled/FillPat.c
-# SRC += libs/OrbitOLED/lib_OrbitOled/OrbitOled.c
-# SRC += libs/OrbitOLED/lib_OrbitOled/OrbitOledChar.c
-# SRC += libs/OrbitOLED/lib_OrbitOled/OrbitOledGrph.c
 
 
-# INCLUDES += -I"$(PROJECT_DIR)/libs/OrbitOLED"
-# INCLUDES += -I"$(PROJECT_DIR)/libs/OrbitOLED/lib_OrbitOled"
+SRC += libs/lib_OrbitOled/ChrFont0.c
+SRC += libs/lib_OrbitOled/delay.c
+SRC += libs/lib_OrbitOled/FillPat.c
+SRC += libs/lib_OrbitOled/OrbitOled.c
+SRC += libs/lib_OrbitOled/OrbitOledChar.c
+SRC += libs/lib_OrbitOled/OrbitOledGrph.c
+
+
+#INCLUDES += -I"$(PROJECT_DIR)/libs/OrbitOLED"
+INCLUDES += -I"$(PROJECT_DIR)/libs/lib_OrbitOled"
