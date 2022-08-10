7a816a9bf458a637a8d53120253adc575b6bdd65
Angus Eason <aje87@uclive.ac.nz>
2022-08-11 10:15:44 +1200

    Wheels part of car_state, UART only updates once per second and only when its values change

--- apps/car/wheels.h
+++ apps/car/wheels.h
@@ -7,18 +7,6 @@
 #include "stdlib.h"
 #include "utils/ustdlib.h"
 
-/**
- * @brief Structure for storing information about car wheels
- * @param turnRadius The turn radius of the wheel (m)
- * @param speed The tangential velocity of the wheel (km/h)
- * @param pulseHz The pulse rate of wheel pwm encoder (Hz) 
- */
-typedef struct {
-    float turnRadius; //m
-    float speed; //km/h
-    float pulseHz; //Hz
-} Wheel;
-
 
 /**
  * @brief Task to update the wheel information and signal to PWM generators to update the frequencies
